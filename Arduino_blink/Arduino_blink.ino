faed695e8b6fffa6d8818f31f056e93fc9aae306
Francesco Crovelli <133510380+francescocrovelli@users.noreply.github.com>
2023-05-14 06:12:10 +0200

    Add files via upload
    Signed-off-by: Francesco Crovelli <133510380+francescocrovelli@users.noreply.github.com>

---
+++ Arduino_blink/Arduino_blink.ino
@@ -0,0 +1,15 @@
+// www.ProgettiArduino.com
+// Arduino blink facciamo lampeggiare un led  
+  
+  #define LED 13            // LED collegato al pin digitale 13  
+  
+void setup() {  
+  pinMode(LED, OUTPUT);     // imposta il pin digitale come output  
+}  
+  
+void loop() {  
+  digitalWrite(LED, HIGH);  // accende il LED  
+  delay(1000);              // aspetta un secondo  
+  digitalWrite(LED, LOW);   // spegne il LED  
+  delay(1000);              // aspetta un secondo  
+}  
