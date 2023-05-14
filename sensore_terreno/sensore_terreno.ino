8e117d926532632c814313834d339a7b45e9269b
Francesco Crovelli <133510380+francescocrovelli@users.noreply.github.com>
2023-05-14 06:46:18 +0200

    Add files via upload
    Signed-off-by: Francesco Crovelli <133510380+francescocrovelli@users.noreply.github.com>

---
+++ sensore_terreno/sensore_terreno.ino
@@ -0,0 +1,16 @@
+const int soglia_critica = 300; //Soglia alla quale il led sarà ON
+void setup()
+{
+Serial.begin(9600);
+pinMode(9, OUTPUT);
+}
+void loop()
+{
+int sensorValue = analogRead(A0); //Legge il valore analogico
+Serial.println(sensorValue); //Stampa a schermo il valore
+if (sensorValue <= soglia_critica)
+digitalWrite(9,HIGH); //Accendi led
+else
+digitalWrite(9,LOW); //Spegni led
+delay(2000); //Attende due secondi
+}
