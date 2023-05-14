9cc5669a16288bd850a191bed221e6f84c78a810
Francesco Crovelli <133510380+francescocrovelli@users.noreply.github.com>
2023-05-14 06:36:03 +0200

    Add files via upload
    Signed-off-by: Francesco Crovelli <133510380+francescocrovelli@users.noreply.github.com>

---
+++ sensore_fiamma/sensore_fiamma.ino
@@ -0,0 +1,29 @@
+int max=20;
+void setup() {
+  pinMode(3,OUTPUT);
+  pinMode(9,OUTPUT);
+  digitalWrite(7,LOW);
+}
+void loop() {
+  if(analogRead(A0)>=max){
+    alarm();
+    flash();
+    digitalWrite(9,HIGH);
+  }
+  else{
+    digitalWrite(9,LOW);
+  }
+}
+void alarm(){
+  tone(4,400,500);
+  delay(500);
+  tone(4,650,500);
+  delay(500);
+}
+void flash(){
+  digitalWrite(3,HIGH);
+  delay(50);
+  digitalWrite(3,LOW);
+}
+
+
