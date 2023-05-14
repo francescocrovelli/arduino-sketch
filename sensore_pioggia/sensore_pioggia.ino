e9e0aa4e72903b397cf63baedfb4998edd6900a0
Francesco Crovelli <133510380+francescocrovelli@users.noreply.github.com>
2023-05-14 06:38:50 +0200

    Add files via upload
    Signed-off-by: Francesco Crovelli <133510380+francescocrovelli@users.noreply.github.com>

---
+++ sensore_pioggia/sensore_pioggia.ino
@@ -0,0 +1,46 @@
+
+
+// ITA: Assegno i pin ai led
+int pinIngresso=2;
+int pinLed=3;
+
+void setup()
+{
+   // ENG: Enable Serial Port (9600 baud)
+   // ITA: Abilito la porta seriale (9600 baud)
+  Serial.begin(9600);
+  
+  // ENG: Setup pins for INPUT and OUTPUT  
+  // ITA: Imposto i pin come INPUT e come OUTPUT
+  pinMode(pinIngresso, INPUT);
+  pinMode(pinLed,OUTPUT);
+  
+}
+
+//ENG: Variable declaration 
+//ITA: Dichiarazione variabili 
+
+int pioggia=0;
+int misurazione=0;
+
+void loop()
+{
+  // ITA: Leggo l'ingresso digitale
+  // ENG: Reading digital input
+  pioggia = digitalRead(pinIngresso);
+  
+  if(pioggia==1) 
+    digitalWrite(pinLed,LOW);
+  if(pioggia==0)
+    digitalWrite(pinLed,HIGH);
+    
+    // ITA: Leggo l'ingresso analogico
+    // ENG: Reading analog input
+    misurazione = analogRead(A0);
+    Serial.println(misurazione);
+  
+  
+  
+  delay(1000);
+  
+}
