12176ae662d8b3a895c41c59476d7c6e9d169ac7
Francesco Crovelli <133510380+francescocrovelli@users.noreply.github.com>
2023-05-14 07:19:00 +0200

    Add files via upload
    Signed-off-by: Francesco Crovelli <133510380+francescocrovelli@users.noreply.github.com>

---
+++ sensore_flex/sensore_flex.ino
@@ -0,0 +1,38 @@
+#include <Servo.h>
+
+
+Servo myservo;
+int pos = 170;
+int pos1 = 20;
+
+int flexSensorPin = A0; //analog pin 0
+
+void setup(){
+  Serial.begin(9600);
+  myservo.attach(9);
+}
+
+void loop(){
+  int flexSensorReading = analogRead(flexSensorPin); 
+  
+  // qui potete vedere da monitor seriale i valori e decidere il grado 
+  // del servo tramite la variabile pos e pos1
+
+  if(flexSensorReading < 80){
+  myservo.write(pos); // tell servo to go to position in variable 'pos' 
+  delay(15);
+  }
+  if(flexSensorReading > 110){
+  myservo.write(pos1); // tell servo to go to position in variable 'pos' 
+  delay(15);
+  }
+
+  Serial.println(flexSensorReading);
+
+
+  int flex0to100 = map(flexSensorReading, 512, 614, 0, 100);
+  Serial.println(flex0to100);
+
+  //delay(250); //just here to slow down the output for easier reading
+}
+
