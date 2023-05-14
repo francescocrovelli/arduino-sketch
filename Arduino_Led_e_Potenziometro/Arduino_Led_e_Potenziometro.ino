be7f098b7e62b4d55f3dd19a9e0fdeb2fa4a70bc
Francesco Crovelli <133510380+francescocrovelli@users.noreply.github.com>
2023-05-14 06:16:18 +0200

    Add files via upload
    Signed-off-by: Francesco Crovelli <133510380+francescocrovelli@users.noreply.github.com>

---
+++ Arduino_Led_e_Potenziometro/Arduino_Led_e_Potenziometro.ino
@@ -0,0 +1,15 @@
+//Arduino Led e potenziometro, Facciamo lampeggiare un led in base al valore dato dal potenziometro
+//Per maggiori info: www.ProgettiArduino.com
+
+int Ledpin = 9; //Il pin a cui è connesso il led 
+
+void setup() { 
+pinMode(Ledpin, OUTPUT); //Il pin è in uscita 
+} 
+
+void loop() { 
+digitalWrite(Ledpin, HIGH); //Il led è acceso 
+delay(analogRead(0)); //Attendi il valore letto dal pin analogico 0 
+digitalWrite(Ledpin, LOW); //Spegni il led 
+delay(analogRead(0)); //Attendi nuovamente il tempo letto dal pin analogico 0 
+}
